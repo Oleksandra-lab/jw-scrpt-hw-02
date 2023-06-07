77de60fcd6a7be4b1c2427577af0b4b215daf0be
Oleksandra-lab <tka4olex@gmail.com>
2023-06-07 16:24:55 +0300

    flesson 3

---
+++ js/script02.js
@@ -0,0 +1,13 @@
+// const genres = ["Jazz", "Blues"]
+// genres.push("Rock and roll")
+// console.log (genres);
+// console.log (genres[0]);
+// console.log (genres[genres.length -1]);
+// console.log (genres.shift());
+// console.log (genres.unshift("Contry", "Reggae"));
+
+
+const fruits = ['apple', 'grape', 'peach', 'banana', 'lemon'];
+
+for (let i = 0; i < fruits.length; i += 1);
+console.log(fruits[i]);
